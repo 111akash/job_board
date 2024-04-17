e5831d8037d7e0dbcac0a0c40a4c6183e97f77bb
akash <akash329@protonmail.com>
2024-04-17 12:52:34 +0530

    added db config

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { global } from "styled-jsx/css";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
