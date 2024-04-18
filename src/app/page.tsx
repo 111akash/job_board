7926763dd87ab6a913b134acd43d387cce27b075
akash <akash329@protonmail.com>
2024-04-18 19:07:26 +0530

    added the jobs list server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import JobListItem from "@/components/JobListItem";
 import prisma from "@/lib/prisma";
 
 export default async function Home() {
@@ -6,5 +7,21 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
 
-  return <main>{JSON.stringify(jobs)}</main>;
+  return (
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Developer jobs
+        </h1>
+        <p className="text-muted-foreground">Find your dream job</p>
+      </div>
+      <section>
+        <div className="space-y-4">
+          {jobs.map((job) => (
+            <JobListItem job={job} key={job.id} />
+          ))}
+        </div>
+      </section>
+    </main>
+  ); 
 }
